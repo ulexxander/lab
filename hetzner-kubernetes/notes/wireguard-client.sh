e7811a0425140406e500d6f9ccc281a016a436fa
Alexander Ustyugov <ulexxander@gmail.com>
2023-07-08 20:17:46 +0200

    patched ingress-nginx manifest (externalIPs)

--- hetzner-kubernetes/notes/wireguard-client.sh
+++ hetzner-kubernetes/notes/wireguard-client.sh
@@ -13,7 +13,7 @@ PrivateKey = $(sudo cat /etc/wireguard/private.key)
 Address = 10.112.32.2/20
 
 [Peer]
-PublicKey = hpdES4TFNJSZRb8oSNmtyfnVrNC15rXzBPUb0Q19JB4=
+PublicKey = qTmUMQe/8m8uaTtlpnXX9xYiXIOmHrh0Y/7Ba/53hAc=
 AllowedIPs = 10.112.0.0/16
 Endpoint = $(terraform output -raw gateway_public_ip):51820
 EOF
